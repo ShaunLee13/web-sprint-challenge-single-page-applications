d4cc5d59b8b4b8011be1e71c7479e56d950b5095
Shaun Lee <shaundlee.0103@gmail.com>
2020-06-19 11:59:49 -0400

    Added components needed for site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { Switch, Route } from 'react-router-dom'
+import Header from "./components/Header";
+
+const initialForm = {
+  name:'',
+  size:'',
+  toppings: {
+    pepperoni: false,
+    blackOlives: false,
+    mushrooms: false,
+    sausage: false
+  }
+}
+
 
 const App = () => {
+  const [ orderForm, newOrderForm ] = useState(initialForm)
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <Header />
+      <Switch>
+        {/* 
+        <Route>
+          <Form path='/form/' orderForm={orderForm}>
+        </Route>
+        
+        <Route>
+          <Home>
+        </Route path='/'> 
+        */}
+      </Switch>
+    </div>
   );
 };
 export default App;
